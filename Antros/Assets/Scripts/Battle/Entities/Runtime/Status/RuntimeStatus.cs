a63800ab0239c96861f26b256a2357ab5d4c91a8
YoannMnts <yoann.montesinos@gmail.com>
2026-06-25 17:00:09 +0200

    RuntimeStatus

---
+++ Antros/Assets/Scripts/Battle/Entities/Runtime/Status/RuntimeStatus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ATCG.Battle.Commands.Core;
+using ATCG.Battle.Commands.Core.Players;
+using ATCG.Battle.Commands.Players;
+using ATCG.Battle.Entities.Components.Status.Signals;
+using UnityEngine;
+
+namespace ATCG.Battle.Entities.Runtime.Status
+{
+    public class RuntimeStatus : MonoBehaviour, ICommandListener<StatusSignal>
+    {
+        private void OnEnable()
+        {
+            this.RegisterListener();
+        }
+
+        private void OnDisable()
+        {
+            this.UnregisterListener();
+        }
+
+        public async Awaitable Play(CommandListenerState state, CommandContext context, StatusSignal command)
+        {
+            await Awaitable.MainThreadAsync();
+            state.CompleteAll(this);
+            var infos = command.GetInfos();
+            
+            int id = infos.id;
+            StatusAction action = infos.action;
+        }
+    }
+}
