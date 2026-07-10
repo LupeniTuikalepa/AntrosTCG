0391ffd86156ef1199a5b6dc38cb91c5340b2565
KaKoKa5 <bonfardeci.saverio@e-artsup.net>
2026-07-10 11:55:18 +0200

    modif

--- Antros/Assets/Scripts/Core/HexGrids/Patterns/Implementations/Trident/TridentPatternData.cs
+++ Antros/Assets/Scripts/Core/HexGrids/Patterns/Implementations/Trident/TridentPatternData.cs
@@ -7,14 +7,14 @@ namespace ATCG.HexGrids.Patterns
 	[Serializable]
 	public class TridentPatternData : PatternData
 	{
-		[field: SerializeField, BoxGroup("Specific"), Min(0)]
+		[field: SerializeField, BoxGroup("Specific"), Min(1)]
 		public int Range { get; private set; }
 
-		[field: SerializeField, BoxGroup("Specific"), Min(1)]
+		[field: SerializeField, BoxGroup("Specific"), Range(1, 6)]
 		[Tooltip("Nombre de branches, doit rester impair pour une fourche symétrique (3, 5, 7...)")]
 		public int BranchCount { get; private set; } = 3;
 
-		[field: SerializeField, BoxGroup("Specific"), Range(20, 300)]
+		[field: SerializeField, BoxGroup("Specific"), Range(0, 90)]
 		[Tooltip("Écart angulaire entre chaque branche. Sera arrondi au multiple de 60° le plus proche (contrainte de la grille hexagonale).")]
 		public int AngleStepDegrees { get; private set; } = 20;
 
