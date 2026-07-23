55e0301cb22696ebb8fb67402aa5a86ea98ec08e
KaKoKa5 <bonfardeci.saverio@e-artsup.net>
2026-07-23 13:44:27 +0200

    avant le rebase

--- Antros/Assets/Scripts/Battle/CapacitySystem/Status/FireStatus/Burn/BurnStatus.cs
+++ Antros/Assets/Scripts/Battle/CapacitySystem/Status/FireStatus/Burn/BurnStatus.cs
@@ -3,6 +3,7 @@ using ATCG.Battle.CapacitySystem.Status.Iterations;
 using ATCG.Battle.Commands;
 using ATCG.Battle.Commands.EntityCommands;
 using ATCG.Battle.Commands.GameCommands.Players;
+using ATCG.Battle.Entities;
 using ATCG.Battle.Entities.Aspects;
 using ATCG.Battle.Entities.Components;
 using ATCG.Battle.Entities.Components.Status;
@@ -10,52 +11,48 @@ using UnityEngine;
 
 namespace ATCG.Battle
 {
-    public partial class BurnStatus : Status<BurnStatusData, BurnStatusComponent, StatusDurationController>, ITickOnTurnBegin
-    {
-	    protected override BurnStatusComponent CreateStatusComponent(BurnStatusData data, in StatusContext context)
-	    {
-		    return new BurnStatusComponent(data);
-	    }
-
-	    protected override StatusDurationController CreateStatusController(BurnStatusData data, in StatusContext context)
-	    {
-		    return new StatusDurationController(data.normalDuration);
-	    }
+	public partial class BurnStatus : Status<BurnStatusData, StatusDurationController>,
+		ITickOnTurnBegin
+	{
+		protected override StatusDurationController CreateStatusController(BurnStatusData data,
+			in StatusContext context)
+		{
+			return new StatusDurationController(data.normalDuration);
+		}
 
 
-	    protected override void OnStack(BurnStatusData data, in EntityStatusInfos statusInfos, in StatusContext context)
-	    {
-		    statusInfos.StatusController.AddOrRemoveTicks(data.normalDuration);
-		    base.OnStack(data, in statusInfos, in context);
-	    }
+		protected override void OnStack(BurnStatusData data, in EntityStatusInfos statusInfos, in StatusContext context)
+		{
+			statusInfos.StatusController.AddOrRemoveTicks(data.normalDuration);
+			base.OnStack(data, in statusInfos, in context);
+		}
 
-	    protected override void OnTick(BurnStatusData data, in EntityStatusInfos statusInfos, in StatusContext context)
-	    {
-		    base.OnTick(data, in statusInfos, context);
-		    int damage = data.Damage * statusInfos.StatusController.RemainingTicks;
+		protected override void OnTick(BurnStatusData data, in EntityStatusInfos statusInfos, in StatusContext context)
+		{
+			base.OnTick(data, in statusInfos, context);
+			int damage = data.Damage * statusInfos.StatusController.RemainingTicks;
 
-		    if (statusInfos.targetAddress.HasComponent<HealthComponent>())
-		    {
-				 Debug.Log(damage + " damage sur " + statusInfos.targetAddress);
-			    DamageCommand damageCommand = new DamageCommand(damage, statusInfos.targetAddress);
-			    damageCommand.Run(context.battlePhase);
-		    }
-		    
-		    if (!statusInfos.targetAddress.TryGetComponentRO<BattleCellComponent>(out _))
-			    return;
+			if (statusInfos.targetAddress.HasComponent<HealthComponent>())
+			{
+				Debug.Log(damage + " damage sur " + statusInfos.targetAddress);
+				DamageCommand damageCommand = new DamageCommand(damage, statusInfos.targetAddress);
+				damageCommand.Run(context.battlePhase);
+			}
 
-		    BattleCellAspect cellAspect = new BattleCellAspect(statusInfos.targetAddress);
-          
-		    foreach (ComponentRef<GridMemberComponent> member in cellAspect.GetMembers())
-		    {
-			    if (!member.EntityAddress.HasComponent<HealthComponent>())
-				    continue;
-              
-			    DamageCommand damageCommand = new DamageCommand(damage, member.EntityAddress);
-			    damageCommand.Run(context.battlePhase);
+			if (!statusInfos.targetAddress.Is(out BattleCellAspect cellAspect))
+				return;
 
-			    Debug.Log($"[BurnStatus] {damage} dégâts de zone infligés à l'entité {member.EntityAddress} sur la case !");
-		    }
-	    }
-    }
+			foreach (ComponentRef<GridMemberComponent> member in cellAspect.GetMembers())
+			{
+				if (!member.EntityAddress.HasComponent<HealthComponent>())
+					continue;
+
+				DamageCommand damageCommand = new DamageCommand(damage, member.EntityAddress);
+				damageCommand.Run(context.battlePhase);
+
+				Debug.Log(
+					$"[BurnStatus] {damage} dégâts de zone infligés à l'entité {member.EntityAddress} sur la case !");
+			}
+		}
+	}
 }
