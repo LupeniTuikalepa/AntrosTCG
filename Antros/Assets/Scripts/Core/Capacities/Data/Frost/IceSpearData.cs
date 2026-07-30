65d0f5a48e8059bfa619ce076f21932f3faecbf8
Lupeni Tuikalepa <lupeni.tuikalepa@gmail.com>
2026-07-30 14:12:38 +0200

    fix capacity editor

---
+++ Antros/Assets/Scripts/Core/Capacities/Data/Frost/IceSpearData.cs
@@ -0,0 +1,12 @@
+using ATCG.Capacities.Attributs;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ATCG.Capacities.Data.Frost
+{
+    [CreateAssetMenu(menuName = "ATCG/Capacities/Frost/IceSpear")]
+    [WithStep("Hit")]
+    public partial class IceSpearData : CapacityData
+    {
+    }
+}
