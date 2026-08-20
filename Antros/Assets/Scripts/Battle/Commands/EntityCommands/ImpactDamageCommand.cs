13c15640eb496b9971c1fdc0270ffa72d8914793
YoannMnts <yoann.montesinos@gmail.com>
2026-08-20 17:16:40 +0200

    PushBackCommand can now make collision

---
+++ Antros/Assets/Scripts/Battle/Commands/EntityCommands/ImpactDamageCommand.cs
@@ -0,0 +1,28 @@
+﻿using ATCG.Battle.Commands.Entities;
+using ATCG.Battle.Commands.Infos;
+using ATCG.Battle.Entities;
+using ATCG.Metrics;
+
+namespace ATCG.Battle.Commands.EntityCommands
+{
+    public class ImpactDamageCommand : EntityCommand<NoInfos>
+    {
+        private readonly EntityAddress other;
+
+        public ImpactDamageCommand(EntityAddress address, EntityAddress other, string source = DEFAULT_SOURCE) : base(address, source)
+        {
+            this.other = other;
+        }
+
+        protected override void Process(in CommandContext context)
+        {
+            var collisionsDamage = GameMetrics.Current.CollisionsDamage;
+            
+            var ownerDamageCommand = new DamageCommand(collisionsDamage, Target.ToAddress(context.World));
+            var otherDamageCommand = new DamageCommand(collisionsDamage, other);
+            
+            Inject(context, ownerDamageCommand);
+            Inject(context, otherDamageCommand);
+        }
+    }
+}
